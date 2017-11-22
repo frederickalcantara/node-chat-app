612b8c2687246b98e942a4f8b352c400fe6701ba
frederickalcantara <frederickaalcantara@gmail.com>
2017-11-22 18:08:38 -0500

    Add css for chat page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -27,13 +27,15 @@ socket.on('newLocationMessage', (message) => {
 });
 
 $('#message-form').on('submit', (e) => {
-    e.preventDefault();
+	e.preventDefault();
+
+	let messageTextbox = $("[name=message]");
 
     socket.emit("createMessage", {
 		from: "User",
-		text: $("[name=message]").val()
+		text: messageTextbox.val()
 	}, () => {
-
+		messageTextbox.val('')
 	});
 });
 
@@ -43,12 +45,16 @@ locationButton.on('click', () => {
 		return alert('Geolocation not supported by your browser.');
 	}
 
+	locationButton.attr('disabled', 'disabled').text('Sending location...');
+
 	navigator.geolocation.getCurrentPosition((position)=> {
+		locationButton.removeAttr('disabled').text('Send location');
 		socket.emit('createLocationMessage', {
 			latitude: position.coords.latitude,
 			longitude: position.coords.longitude
 		});
 	}, () => {
+		locationButton.removeAttr("disabled").text("Send location");
 		alert('Unable to fetch location');
 	});
 });
