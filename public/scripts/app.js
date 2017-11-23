6e84ac9a99763ed576420375a8bd7fdc3d820022
frederickalcantara <frederickaalcantara@gmail.com>
2017-11-22 21:23:11 -0500

    Scroll to bottom if user is close to bottom

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,21 @@
 let socket = io();
 
+let scrollToBottom = () => {
+	// Selectors
+	let messages = $('#messages');
+	let newMessage= messages.children('li:last-child');
+	// Heights
+	let clientHeight = messages.prop('clientHeight');
+	let scrollTop = messages.prop('scrollTop');
+	let scrollHeight = messages.prop('scrollHeight');
+	let newMessageHeight = newMessage.innerHeight();
+	let lastMessageHeight = newMessage.prev().innerHeight();
+
+	if (clientHeight + scrollTop + lastMessageHeight + newMessageHeight >= scrollHeight) {
+		messages.scrollTop(scrollHeight);
+	}
+}
+
 socket.on("connect", () => {
   console.log("Connected to server");
 });
@@ -18,6 +34,7 @@ socket.on('newMessage', (message) => {
 	});
 
 	$('#messages').append(html);
+	scrollToBottom();
 });
 
 socket.on('newLocationMessage', (message) => {
@@ -29,7 +46,8 @@ socket.on('newLocationMessage', (message) => {
 		createdAt: formattedTime
 	});
 
-  	$("#messages").append(html);
+	$("#messages").append(html);
+	scrollToBottom();
 });
 
 $('#message-form').on('submit', (e) => {
